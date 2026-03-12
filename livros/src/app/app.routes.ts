f994aec8ed776a301c7ddfb7a1e8e899c61650a7
franklin <frank.fkn.23@gmail.com>
2026-03-12 01:11:16 -0300

    criando componente de livros e configurando suas rotas

--- livros/src/app/app.routes.ts
+++ livros/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Livros } from './livros/livros';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'livros', component: Livros },
+  { path: '', component: Livros },
+];
