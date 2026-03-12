1bd079fbdb159fa39a8f63f36e9b4d763c87e7e7
franklin <frank.fkn.23@gmail.com>
2026-03-12 02:18:26 -0300

    adicionando elemento livro na interface

--- livros/src/app/livros/livros.ts
+++ livros/src/app/livros/livros.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { LIVROS } from '../database/livros';
+import { Livro } from '../model/livro';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-livros',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './livros.html',
   styleUrl: './livros.css',
 })
-export class Livros {}
+export class Livros {
+  livros: Livro[] = LIVROS;
+}
