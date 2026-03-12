fc38af43d2e2edd051e97653a35218f1a965d61c
franklin <frank.fkn.23@gmail.com>
2026-03-12 02:16:17 -0300

    criando database temporario de livros

---
+++ livros/src/app/database/livros.ts
@@ -0,0 +1,7 @@
+import { Livro } from '../model/livro';
+
+export const LIVROS: Livro[] = [
+  { titulo: 'Origem', autor: 'Dan Brown', quantidadePaginas: 300 },
+  { titulo: 'Jogador número 1', autor: 'Enerst Cline', quantidadePaginas: 438 },
+  { titulo: 'A batalha do apocalipse', autor: 'Eduardo Spohr', quantidadePaginas: 423 },
+];
