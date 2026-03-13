85ae3c644e600cf0e146c3f316103539abf926ac
franklin <frank.fkn.23@gmail.com>
2026-03-13 07:38:46 -0300

    adicionando item à lista de livros

--- livros/src/app/database/livros.ts
+++ livros/src/app/database/livros.ts
@@ -1,7 +1,23 @@
 import { Livro } from '../model/livro';
 
 export const LIVROS: Livro[] = [
-  { titulo: 'Origem', autor: 'Dan Brown', quantidadePaginas: 300 },
-  { titulo: 'Jogador número 1', autor: 'Enerst Cline', quantidadePaginas: 438 },
-  { titulo: 'A batalha do apocalipse', autor: 'Eduardo Spohr', quantidadePaginas: 423 },
+  { titulo: 'Origem', autor: 'Dan Brown', capa: 'origem', quantidadePaginas: 300 },
+  {
+    titulo: 'Jogador número 1',
+    autor: 'Enerst Cline',
+    capa: 'jogador-numero-1',
+    quantidadePaginas: 438,
+  },
+  {
+    titulo: 'A batalha do apocalipse',
+    autor: 'Eduardo Spohr',
+    capa: 'a-batalha-do-apocalipse',
+    quantidadePaginas: 423,
+  },
+  {
+    titulo: 'Mistborn - Nascidos da bruma',
+    autor: 'Brandon Sanderson',
+    capa: 'mistborn-nascidos-da-bruma',
+    quantidadePaginas: 1267,
+  },
 ];
